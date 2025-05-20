57d574d316f41aaafc3d6c4813e29bec38a07ede
Frostic21 <145382864+Frostic21@users.noreply.github.com>
2025-05-20 09:15:03 -0700

    Starting

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,46 @@
-const { User } = require('../models');
-const md5 = require('md5')
+const {User} = require('../models');
+const md5 = require('md5');
+const passport = require('passport');
+
+module.exports.renderRegistration= function(req,res){
+    res.render('user/register');
+}
+
+module.exports.register = async function(req,res){
+    const existingUser = await User.findOne({
+        where:{
+            email: req.body.email
+        }
+    });
+    if(existingUser){
+        res.render('user/register',{
+            error: 'User Already Exists'
+        })
+    } else {
+        await User.create({
+            email: req.body.email,
+            password: md5(req.body.password),
+            name: req.body.name,
+        });
+        res.redirect('/');
+    }
+}
+
+module.exports.renderLogin = function(req,res){
+    let error = null
+    if(req.session.messages && req.session.messages.length > 0){
+        error = req.session.messages[0];
+    }
+    res.render('user/login', {error});
+}
+
+module.exports.authenticate = passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureMessage: true
+});
+
+module.exports.logout = function(req, res){
+    req.logout();
+    res.redirect('/login');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const userController = require('../controllers/userController')
+
+
+function addUserToViews(req, res, next) {
+  if (req.user) {
+    res.locals.user = req.user;
+  }
+  next();
+}
+
+function redirectGuests(req, res, next) {
+  if (!req.user) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+}
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', addUserToViews, function(req, res, next) {
+  res.render('index');
 });
+router.get('/taco', addUserToViews, function(req, res, next) {
+  res.render('taco/taco')
+})
+router.get('/register', addUserToViews, userController.renderRegistration);
+router.post('/register', addUserToViews, userController.register);
+
+router.get('/login', addUserToViews, userController.renderLogin);
+router.post('/login', addUserToViews, userController.authenticate);
+
+router.get('/logout', addUserToViews, userController.logout);
 
 module.exports = router;
